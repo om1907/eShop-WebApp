beb36be940ac189236d8549d98ba4b9ec2cc17ea
om1907 <98704784+om1907@users.noreply.github.com>
2024-08-11 16:03:00 +0530

    moved server side code to a single folder

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const colors=require('colors');
+const dotenv=require('dotenv');
+const connectDB=require('./config/db');
+const morgan = require('morgan');
+const authRoute=require('../server/routes/authRoutes')
+const cors =require('cors');
+
+//config env
+dotenv.config();
+
+//Database config
+connectDB();
+
+//rest object
+const app=express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+//routes
+app.use('/api/v1/auth',authRoute);
+
+
+//rest api
+app.get('/',(req,res)=>{
+    res.send({
+        message:'Welcome to eShop app'
+    })
+})
+
+//Port
+const PORT=process.env.PORT||4000
+
+
+//run listen
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`.bgCyan.white)
+})
